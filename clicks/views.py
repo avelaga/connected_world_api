44e526aa4cd9c03f3c7bf2af7e4121f72aee4490
Abhi Velaga <abhinav.velaga@gmail.com>
2021-04-22 00:56:51 -0500

    fixed save bug

--- clicks/views.py
+++ clicks/views.py
@@ -5,5 +5,5 @@ from .models import Click
 
 def index(request):
   click = Click(date="124432", time="12312", x=4,y=4,hue=34)
-  Click.save()
+  click.save()
   return HttpResponse("Hello, world. You're at the clicks index.")
