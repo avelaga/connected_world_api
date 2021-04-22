20b566754e42d44beb926a4a6cc3bfc07afe48b5
Abhi Velaga <abhinav.velaga@gmail.com>
2021-04-22 12:16:27 -0500

    auto date and time

--- clicks/models.py
+++ clicks/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Click(models.Model):
-  date = models.CharField(max_length=200, null=True)
-  time = models.CharField(max_length=200, null=True)
+  date = models.DateField(auto_now=True)
+  time = models.TimeField(auto_now=True)
   x = models.IntegerField()
   y = models.IntegerField()
   hue = models.IntegerField()
--- clicks/views.py
+++ clicks/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 from .models import Click
 
+import json
 
+@csrf_exempt
 def index(request):
-  click = Click(date="124432", time="12312", x=4,y=4,hue=34)
+  if request.method == 'POST':
+    data = request.body.decode('utf-8')
+    data_json = json.loads(data)
+    print(data_json)
+  click = Click(x=4,y=4,hue=34)
   click.save()
   return HttpResponse("Hello, world. You're at the clicks index.")
