926fdd1d2f10ed7061fa4d1c75aae559989bc061
Abhi Velaga <abhinav.velaga@gmail.com>
2021-05-06 13:01:51 -0500

    sqllite

--- connected_world_api/connected_world_api/settings.py
+++ connected_world_api/connected_world_api/settings.py
@@ -88,17 +88,23 @@ WSGI_APPLICATION = 'connected_world_api.wsgi.application'
 #     }
 # }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres',
+#         'USER': 'postgres',
+#         'PASSWORD': 'postgres',
+#         'HOST': 'db',
+#         'PORT': 5432,
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'db',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mydatabase',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
